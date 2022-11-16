3e63905c2073f27c73bba8253b1848297674d333
jain0122 <jain0122@umn.edu>
2022-11-15 19:13:29 -0600

    Added Quickhull

--- algorithms.py
+++ algorithms.py
@@ -239,3 +239,75 @@ def chan(S: List[Point]) -> List[Point]:
         if L != []:
             return L
     return []  # Error case, should never be reached
+
+
+
+from typing import List
+from primitives import *
+
+
+def leftmost(S: List[Point]) -> Point:
+    left = S[0]
+    for p in S[1:]:
+        if p.x < left.x:
+            left = p
+
+    return left
+
+
+def rightmost(S: List[Point]) -> Point:
+    right = S[0]
+    for p in S[1:]:
+        if p.x > right.x:
+            right = p
+
+    return right
+
+
+def quickhull(S: List[Point]) -> List[Point]:
+    P = []
+    a = leftmost(S)
+    b = rightmost(S)
+    S1 = []
+    S2 = []
+    for p in S:
+        if sidedness_i(DLine(a, b), p) == -1:
+            S1.append(p)
+        elif sidedness_i(DLine(b, a), p) == -1:
+            S2.append(p)
+    P.append(a)
+    DD = findquickhull(S1, a, b)
+    if len(DD) > 0:
+        P = P+DD
+    P.append(b)
+    DD = findquickhull(S2, b, a)
+    if len(DD) > 0:
+        P = P+DD
+    return P
+
+
+def findquickhull(S: List[Point], a, b) -> List[Point]:
+    P = []
+    if len(S) == 0:
+        return []
+    else:
+        farthest = S[0]
+        for p in S:
+            if area(p, a, b) > area(farthest, a, b):
+                farthest = p
+        S1 = []
+        S2 = []
+        for p in S:
+            if sidedness_i(DLine(a, farthest), p) == -1:
+                S1.append(p)
+            elif sidedness_i(DLine(farthest, b), p) == -1:
+                S2.append(p)
+
+        DD = findquickhull(S1, a, farthest)
+        if len(DD) > 0:
+            P = P+DD
+        P.append(farthest)
+        DD = findquickhull(S2, farthest, b)
+        if len(DD) > 0:
+            P = P+DD
+        return P
