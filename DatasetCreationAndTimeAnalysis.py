ea2e91be04785393cbdcb04619e64fc8a9e78d5f
jain0122 <jain0122@umn.edu>
2022-11-21 12:57:51 -0600

    Included three real world datasets

--- DatasetCreationAndTimeAnalysis.py
+++ DatasetCreationAndTimeAnalysis.py
@@ -1,9 +1,13 @@
-from Algorithm import *
+from algorithms import *
+from animation_api import *
 import random
 import math
 import pickle
 import time
 import numpy as np
+import json
+import matplotlib.pyplot as plt
+
 
 # def CreateSyntheticCircleDataset(n):
 #     # Creates a synthetic dataset of n points where all the points lie on the circle
@@ -22,11 +26,12 @@ def CreateCircleDataset(n, h):
     # This dataset will be output sensitive if h<<n
     # This dataset will NOT be output sensitive if h=n
     S = []
+    scale = math.sqrt(1)
     for i in range(h):
         x = -1+2*random.random()
         y = -1+2*random.random()
         mag = math.sqrt(x**2 + y**2)
-        S.append(Point(x/mag, y/mag))
+        S.append(Point(x*scale/mag, y*scale/mag))
     for i in range(n-h):
         r = 0.99*math.sqrt(random.random())
         angle = random.random()
@@ -35,6 +40,68 @@ def CreateCircleDataset(n, h):
         S.append(Point(x, y))
 
     return S
+
+def btbDataset():
+    # Real Dataset 1: This dataset contains the locations of the farms where bovine tuberculosis was detected along with the
+    # years in which it was detected and the "spoligotype" of the tuberculosis
+    S = []
+    S9, S12, S15, S20 = [], [], [], []
+    with open("btb.json") as f:
+        L = json.load(f)
+    for i in range(len(L[0])):
+        eval('S'+L[2][i]['spoligotype']+'.append(Point(L[0][i],L[1][i]))')
+
+    (fig, ax) = new_plot()
+    plot_points(S9, ax, c="g", wait=0, label = 'spoligotype: 9')
+    plot_points(S12, ax, c="b", wait=0, label = 'spoligotype: 12')
+    plot_points(S15, ax, c="r", wait=0, label = 'spoligotype: 15')
+    plot_points(S20, ax, c="k", wait=0, label = 'spoligotype: 20')
+    plt.title("Bovine Tuberculosis Dataset")
+    plt.xlabel("x-coordinate")
+    plt.ylabel("y-coordinate")
+    plt.legend(loc="upper left")
+    plt.show()
+
+    return S9+S12+S15+S20
+
+
+
+def beiDataset():
+    # Real Dataset 2: Location of 3605 trees in a tropical rain-forest
+    S = []
+    with open("bei.json") as f:
+        L = json.load(f)
+    for i in range(len(L[0])):
+        S.append(Point(L[0][i],L[1][i]))
+
+    (fig, ax) = new_plot()
+    plot_points(S, ax, c="g", wait=0)
+    plt.title("Beilschmiedia data")
+    plt.xlabel("x-coordinate")
+    plt.ylabel("y-coordinate")
+    plt.show()
+
+    return S
+
+
+def mucosaDataset():
+    # Real Dataset 3: Location of two types of cells in the gastric mucosa of a rat
+    SECL, Sother = [], []
+    with open("mucosa.json") as f:
+        L = json.load(f)
+    for i in range(len(L[0])):
+        eval('S'+L[2][i]+'.append(Point(L[0][i],L[1][i]))')
+
+    (fig, ax) = new_plot()
+    plot_points(SECL, ax, c="g", wait=0, label = 'ECL')
+    plot_points(Sother, ax, c="b", wait=0, label='other')
+    plt.title("Mucosa data")
+    plt.xlabel("x-coordinate")
+    plt.ylabel("y-coordinate")
+    plt.legend()
+    plt.show()
+
+    return SECL+Sother
 
 
 def saveDataset(filename, data):
@@ -58,6 +125,9 @@ def CalculateAvgTime(whichAlgorithm, n, h, max_iter):
 
     for i in range(max_iter):
         S = CreateCircleDataset(n, h)
+        # S = btbDataset()
+        # S = beiDataset()
+        # S = mucosaDataset()
         print('iter = ', i)
         for j in range(len(whichAlgorithm)):
             start = time.time()
@@ -70,12 +140,22 @@ def CalculateAvgTime(whichAlgorithm, n, h, max_iter):
 
 if __name__ == '__main__':
 
-    whichAlgorithm = ['divideConquer0', 'jarvis', 'andrew', 'quickhull']
-    T = CalculateAvgTime(whichAlgorithm, 100000, 3, 10)
+    whichAlgorithm = ['divideConquer_unsorted', 'jarvis', 'andrew', 'quickhull', 'chan']
+    T = CalculateAvgTime(whichAlgorithm, 1000, 3, 1)
     print(T)
+
+
 
     # Example of using save and load functionality
     # S = CreateCircleDataset(10000,100)
     # saveDataset('myData', S)
     # SS = loadDataset('myData')
     # scttr(SS)
+
+
+    S = mucosaDataset()
+
+
+
+
+
